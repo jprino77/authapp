833fc2c7204bf825aa8a940e04dda1fc94431e05
Julian Prino <julianprino@gmail.com>
2019-09-03 23:27:59 -0300

    Auth0 login

---
+++ src/app/components/protegida/protegida.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+  selector: "app-protegida",
+  templateUrl: "./protegida.component.html",
+  styleUrls: ["./protegida.component.css"]
+})
+export class ProtegidaComponent implements OnInit {
+  constructor(public auth: AuthService) {}
+
+  ngOnInit() {
+    console.log("ngOnInit protegida");
+    this.auth.userProfile$.subscribe(perfil => {
+      console.log(perfil);
+    });
+  }
+}
